02c5ffd4c06d789100bdf14be66ce8d1c9a87fc4
alt757 <hendricksonlucas29@gmail.com>
2026-07-14 11:29:27 -0500

    Correct activities array syntax in bot.js
    Fix syntax for activities array in bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -21,7 +21,7 @@ export const botConfig = {
     // 3 = Watching
     // 4 = Custom
     // 5 = Competing
-    activities: [3 Chill Server
+    activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
         state: "Managing Chill Server ",     // this is what people actually see
